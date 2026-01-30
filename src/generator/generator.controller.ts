632599d3ba832bfd6a2c46490cf309e6b0ab87fa
ntwali-1 <ntwaliyanis@gmail.com>
2026-01-30 17:47:58 +0200

    refactor: generator-module

--- src/generator/generator.controller.ts
+++ src/generator/generator.controller.ts
@@ -7,8 +7,6 @@ import { GenerateDto } from '../templates/dto/template.dto';
 export class GeneratorController {
   constructor(private readonly generatorService: GeneratorService) {} 
 
-
-  //Generate a pdf or image
   @Post()
   @UsePipes(new ValidationPipe({ transform: true }))
   async generate(@Body() generateDto: GenerateDto, @Res() res: Response) {
